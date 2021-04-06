81ca211170ec88e89eaefbcc6e4150922ceca6d9
priyanka chilakala <priyankachilakala510@gmail.com>
2021-04-06 17:04:06 +0530

    changes for voting app

--- my-app/src/components/CreateVote.js
+++ my-app/src/components/CreateVote.js
@@ -7,15 +7,15 @@ const CreateVote = ({onChangeForm, createVote }) => {
     return(
         <div className="container">
             <div className="row">
-                <div className="col-md-7 mrgnbtm">
-                <h2>Create Vote</h2>
+                <div className="col-md-7">
+                <h2>Voting Form</h2>
                 <form>
                     <div className="row">
-                        <div className="form-group col-md-6">
+                        <div className="form-group col-md-12">
                             <label htmlFor="exampleInputEmail1">First Name</label>
                             <input type="text" onChange={(e) => onChangeForm(e)}  className="form-control" name="firstname" id="firstname" aria-describedby="emailHelp" placeholder="First Name" />
                         </div>
-                        <div className="form-group col-md-6">
+                        <div className="form-group col-md-12">
                             <label htmlFor="exampleInputPassword1">Last Name</label>
                             <input type="text" onChange={(e) => onChangeForm(e)} className="form-control" name="lastname" id="lastname" placeholder="Last Name" />
                         </div>
@@ -26,7 +26,7 @@ const CreateVote = ({onChangeForm, createVote }) => {
                             <input type="text" onChange={(e) => onChangeForm(e)} className="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Email" />
                         </div>
                     </div>
-                    <button type="button" onClick= {(e) => createVote()} className="btn btn-danger">Create</button>
+                    <button type="button" onClick= {(e) => createVote()} className="btn btn-primary">Record Vote</button>
                 </form>
                 </div>
             </div>
